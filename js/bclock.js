39d61d908c18418ce46c8e53a0c10d6bafb8d1e9
kumanofoo <kumanofoo@gmail.com>
2024-11-11 11:46:23 +0900

    fix: layout

---
+++ js/bclock.js
@@ -0,0 +1,21 @@
+const bclock = () => {
+  const d = new Date();
+  let year = d.getFullYear();
+  let month = ('00' + d.getMonth()).slice(-2);
+  let date = ('00' + d.getDate()).slice(-2);
+  let day = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"][d.getDay()];
+  let hour = ('00' + d.getHours()).slice(-2);
+  let min = ('00' + d.getMinutes()).slice(-2);
+  let sec = ('00' + d.getSeconds()).slice(-2);
+
+  let today = `${year}.${month}.${date} ${day}`;
+  let time = `${hour}:${min}:${sec}`;
+
+  document.querySelector(".bclock-date").innerText = today;
+  document.querySelector(".bclock-time").innerText = time;
+}
+
+window.onload = () => {
+  console.log("hello onload");
+  setInterval(bclock, 1000);
+}
